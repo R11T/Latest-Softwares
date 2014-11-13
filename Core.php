88b13272535803373c9af1d435b2f9855890c7d2
Romain Lecault <romain.lecault@comalia.com>
2014-11-13 09:57:28 +0100

    Adding composer

--- Core.php
+++ Core.php
@@ -4,6 +4,3 @@ define('ROOT_DIR', __DIR__ . '/');
 
 // Project's constants declaration and init
 
-spl_autoload_register(function ($class) {
-        require_once str_replace('\\', '/', $class) . '.php';
-});
