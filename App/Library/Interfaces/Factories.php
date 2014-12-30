6ad7774e945c929066cc0d152476ea5eed64e115
Romain Lecault <romain.lecault@comalia.com>
2014-12-30 15:13:27 +0100

    And again

--- App/Library/Interfaces/Factories.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Library\Interfaces;
-
-interface Factory
-{
-    public function getSoftwares();
-}
--- App/Model/Software.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\Model;
-
-abstract class Software
-{
-}
