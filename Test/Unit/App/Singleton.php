8ba024c9669639cc992e7a5d26ea39fb1b87f475
Prytoegrian <norrin@trou-noir.fr>
2015-01-04 22:52:29 +0100

    Summary licence

--- Test/Unit/App/Singleton.php
+++ Test/Unit/App/Singleton.php
@@ -1,12 +1,19 @@
 <?php
 /**
  * @licence GPL-v2
+ * Summary :
+ * « You may copy, distribute and modify the software as long as
+ * you track changes/dates of in source files and keep all modifications under GPL.
+ * You can distribute your application using a GPL library commercially,
+ * but you must also disclose the source code. »
+ *
+ * @link https://www.tldrlegal.com/l/gpl2
+ * @see LICENSE file
  */
 namespace Test\Unit\App;
 
 use \atoum;
 use \App\Singleton as _Singleton;
-use \App\Main;
 
 /**
  * Unit testing on the singleton collector
