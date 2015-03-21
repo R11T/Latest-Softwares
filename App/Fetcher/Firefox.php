3f3947fb92697722339664ba162c59808c5c56b8
Prytoegrian <norrin@trou-noir.fr>
2015-03-21 20:57:09 +0100

    WIP fetching firefox data

--- App/Fetcher/Firefox.php
+++ App/Fetcher/Firefox.php
@@ -1,25 +1,162 @@
 <?php
-namespace App\Model\Browser;
+/**
+ * @licence GPL-v2
+ * Summary :
+ * « You may copy, distribute and modify the software as long as
+ * you track changes/dates of in source files and keep all modifications under GPL.
+ * You can distribute your application using a GPL library commercially,
+ * but you must also disclose the source code. »
+ *
+ * @link https://www.tldrlegal.com/l/gpl2
+ * @see LICENSE file
+ */
+namespace App\Fetcher;
 
-class Firefox implements \App\Library\Interfaces\Software
+use \App\Library\Interfaces\IFetchable;
+
+
+/**
+ * Fetcher of all fresh data
+ *
+ * @since 0.6
+ * @author Romain L.
+ * @see \Test\Unit\App\Fetcher\Firefox
+ */
+class Firefox implements IFetchable
 {
-    private $link = 'blablabla';
+    /**
+     * URI
+     *
+     * @var string
+     *
+     * @access private
+     * @see https://en.wikipedia.org/wiki/Uniform_resource_identifier
+     */
+    private $resourceLink = 'http://en.wikipedia.org/w/index.php?action=raw&title=Template:Latest_stable_software_release/Firefox';
 
-    private $regexReleaseVersion = '';
+    /**
+     * Data
+     *
+     * @var string
+     *
+     * @access private
+     */
+    private $data;
 
-    private $regexReleaseDate = '';
-
-    public function getLink()
+    /**
+     * Fetch data
+     *
+     * @return void
+     * @access public
+     * @throws Exception if resource doesn't exist
+     */
+    public function fetchData()
     {
-        return $this->link;
+        $resource = $this->getResourceLink();
+        if ($fileHandle = @fopen($resource, 'rb')) {
+            $content = stream_get_contents($fileHandle);
+            fclose($fileHandle);
+            //var_dump('content', $content);
+            $this->setData($content);
+        } else {
+            throw new \Exception('Data file doesn\'t exist');
+        }
     }
 
-    public function setReleaseVersion()
+    /**
+     * Getter
+     *
+     * @return string
+     * @access public
+     */
+    public function getResourceLink()
     {
+        return DATA_TEST_DIR . 'firefox';
+        // eventually with a switch following virtual mode existence, or something else
+        //return $this->resourceLink; Set this when we'll be in prod, not in current dev
     }
 
+    /**
+     * Setter
+     *
+     * @param string $data
+     *
+     * @return void
+     * @access public
+     */
+    public function setData($data)
+    {
+        $this->data = strtolower(str_replace("\n", '', $data));
+    }
+
+    /**
+     * Getter
+     *
+     * @return string
+     * @access public
+     */
     public function getData()
     {
-        return rand(0, 100);
+        return $this->data;
+    }
+
+    /**
+     * Returns a collection of release items
+     *
+     * @return \App\Library\Collection\Release
+     * @access public
+     */
+    public function fetchRelease()
+    {
+        $collection = null;
+        foreach ($this->fetchPlatform() as $platform) {
+            $data = [
+                'platform'  => $platform,
+                'timestamp' => $this->fetchReleaseTimestamp(),
+                'major'     => $this->fetchReleaseMajor(),
+                'minor'     => $this->fetchReleaseMinor(),
+                'patch'     => $this->fetchReleasePatch(),
+            ];
+            $release = new \App\Item\Release($data);
+            if (null == $collection) {
+                $collection = new \App\Library\Collection\Release($release);
+            } else {
+                $collection->push($release);
+            }
+        }
+        return $collection;
+        $this->fetchPlatform();
+    }
+
+    private function fetchReleaseTimestamp()
+    {
+        return time();
+    }
+
+    private function fetchReleaseMajor()
+    {
+        return 2;
+    }
+    private function fetchReleaseMinor()
+    {
+        return 2;
+    }
+    private function fetchReleasePatch()
+    {
+        return 2;
+    }
+    public function fetchPlatform()
+    {
+        /* Since information isn't set, sending an arbitrary : */
+        return ['windows', 'os x', 'linux'];
+    }
+
+    public function fetchDeveloper()
+    {
+        return 'Mozilla Foundation';
+    }
+
+    public function fetchCommercialName()
+    {
     }
 }
---
+++ Test/Unit/App/Fetcher/Firefox.php
@@ -0,0 +1,16 @@
+<?php
+namespace Test\Unit\App\Fetcher;
+
+use \Test\Unit\TestCase;
+use \App\Fetcher\Firefox as _Firefox;
+
+/**
+ * Unit testing on firefox fetcher
+ *
+ * @since 0.6
+ * @author Romain L.
+ * @see \App\Fetcher\Firefox
+ */
+class Firefox extends TestCase
+{
+}
