c1cfd616fbdf5c0a2be28dc01f5846da3145d92c
Prytoegrian <norrin@trou-noir.fr>
2015-03-29 23:12:42 +0200

    Unit testing Dao\Browser

--- Test/Unit/App/Library/Dao/Browser.php
+++ Test/Unit/App/Library/Dao/Browser.php
@@ -25,7 +25,99 @@ use \App\Library\Dao\Browser as _Browser;
  */
 class Browser extends TestCase
 {
-    public function testUpdateOne()
+    /**
+     * PDO result object
+     *
+     * @var \mock\PDOStatement
+     *
+     * @access private
+     */
+    private $result;
+
+    /**
+     * Tested class
+     *
+     * @var \App\Library\Dao\Browser
+     *
+     * @access private
+     */
+    private $browser;
+
+    /**
+     * Item to save
+     *
+     * @var \mock\App\Item\Browser
+     *
+     * @access private
+     */
+    private $item;
+
+    /**
+     * Setting routine
+     *
+     * @return void
+     * @access public
+     */
+    public function beforeTestMethod()
     {
+        $this->result = new \mock\PDOStatement;
+        $this->mockGenerator()->orphanize('__construct');
+        $db = new \mock\App\Library\Db;
+        $db->getMockController()->prepare = $this->result;
+        $db->getMockController()->beginTransaction = true;
+        $db->getMockController()->commit = true;
+
+        \App\Singleton::db($db);
+        $this->browser = new _Browser;
+        $this->mockGenerator()->orphanize('__construct');
+        $this->item = new \mock\App\Item\Browser;
+    }
+
+    public function testGetType()
+    {
+        $type = $this->browser->getType();
+
+        $this->string($type)->isIdenticalTo('browser');
+    }
+
+    /**
+     * Tests updating one browser with name unknown
+     *
+     * @return void
+     * @access public
+     */
+    public function testUpdateOneWithoutResult()
+    {
+        $this->result->getMockController()->fetch = false;
+
+        $this->exception(function () {
+            $this->browser->updateOne($this->item);
+        })->isInstanceOf('\PDOException');
+    }
+
+    public function testUpdateOneWithResult()
+    {
+        $dataResult = [
+            'software_id' => 42,
+            'platform_id' => 161,
+        ];
+        $dataRelease = [
+            'a' => [
+                'major'     => 2,
+                'minor'     => 7,
+                'patch'     => 1,
+                'timestamp' => 314,
+            ]
+        ];
+        $this->mockGenerator()->orphanize('__construct');
+        $release = new \mock\App\Item\Release;
+        $release->getMockController()->display = $dataRelease;
+        $this->item->getMockController()->getRelease = $release;
+        $this->item->getMockController()->getCommercialName = 'Times';
+        $this->result->getMockController()->fetch = $dataResult;
+
+        $this->when(function () {
+            $this->browser->updateOne($this->item);
+        })->error()->notExists();
     }
 }
